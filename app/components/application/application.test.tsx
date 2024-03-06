39131ba0322e852f781337f2f0c231affdf7cbbd
mohammadnuridin <developer.mohammadnuridin@gmail.com>
2024-03-06 21:53:01 +0800

    getByLabelText

--- app/components/application/application.test.tsx
+++ app/components/application/application.test.tsx
@@ -5,34 +5,16 @@ describe('Application', () => {
     test('Application component rendered', () => {
         render(<Application/>)
 
-        const headingElemenetH1 = screen.getByRole("heading",{level:1})
-        expect(headingElemenetH1).toBeInTheDocument()
+        const bioLabel =  screen.getByLabelText("Bio")
+        expect(bioLabel).toBeInTheDocument()
 
 
-        const headingElemenetH2 = screen.getByRole("heading", {name:'Section 1'})
-        expect(headingElemenetH2).toBeInTheDocument()
+        const inputLabel =  screen.getByLabelText("Name", {selector:"input"})
+        expect(inputLabel).toBeInTheDocument()
 
-        const nameELement = screen.getByRole("textbox",{
-            name:"Name"
-        })
-        expect(nameELement).toBeInTheDocument()
+        const selectLabel =  screen.getByLabelText("Name Country", {selector:"select"})
+        expect(selectLabel).toBeInTheDocument()
 
-
-        const bioELement = screen.getByRole("textbox",{
-            name:"Bio"
-        })
-        expect(bioELement).toBeInTheDocument()
-
-
-        const jobLocation = screen.getByRole("combobox")
-        expect(jobLocation).toBeInTheDocument()
-
-
-        const termsElement = screen.getByRole("checkbox")
-        expect(termsElement).toBeInTheDocument()
-
-
-        const submitButton = screen.getByRole("button")
-        expect(submitButton).toBeInTheDocument()
+        
     });
 });
--- app/components/application/application.tsx
+++ app/components/application/application.tsx
@@ -24,7 +24,7 @@ export const Application = () => {
             <textarea id="bio" />
           </div>
           <div>
-            <label htmlFor="job-location">Job location</label>
+            <label htmlFor="job-location">Name Country</label>
             <select id="job-location">
               <option value="">Select a country</option>
               <option value="US">United States</option>
